d6abbf3ffd1caecd3171b705cacfb769f011f2d4
schulzp <schulzp@users.noreply.github.com>
2020-03-29 12:43:19 +0200

    add check test (mapping test parameters manually)

--- src/test/java/com/example/ExampleResourceTest.java
+++ src/test/java/com/example/ExampleResourceTest.java
@@ -5,6 +5,7 @@ import java.util.stream.Stream;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,14 +18,25 @@ class ExampleResourceTest {
 
     @ParameterizedTest
     @MethodSource("args")
-    void testHelloEndpoint(Map<String, Matcher<?>> tests) {
+    void testParameterized(Map<String, Matcher<?>> tests) {
         tests.forEach((path, matcher) -> {
-            given()
-              .when().get(path)
-              .then()
-                 .statusCode(200)
-                 .body(matcher);
+            assertResponse(path, matcher);
         });
+    }
+
+    @Test
+    void testManually() {
+        args()
+                .flatMap(arguments -> ((Map<String, Matcher<?>>) arguments.get()[0]).entrySet().stream())
+                .forEach(arguments -> assertResponse(arguments.getKey(), arguments.getValue()));
+    }
+
+    void assertResponse(String path, Matcher<?> matcher) {
+        given()
+                .when().get(path)
+                .then()
+                .statusCode(200)
+                .body(matcher);
     }
 
     static Stream<Arguments> args() {
